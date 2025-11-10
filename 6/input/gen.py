8257a1ec9f40b06f4b0fd30abad540e3fff31044
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2025-11-10 16:41:28 +0900

    new input

--- 6/input/gen.py
+++ 6/input/gen.py
@@ -1,6 +1,7 @@
 from random import randint
 
 files = 50
+count = 0
 
 LANGUAGE = "abcdefghijklmnopqrstuvwxyz-"
 
@@ -9,11 +10,19 @@ def get_random_word(length):
     return word
 
 def main():
+    global count
     for i in range(files):
-        with open(f"./random-{i}.txt", "w") as f:
+        with open(f"./random-{count}.txt", "w") as f:
             n = randint(1, 30)
             words = [get_random_word(randint(1, 30)) for _ in range(n)]
             f.write("\n".join(words))
+        count += 1
+    for i in range(10):
+        with open(f"./random-{count}.txt", "w") as f:
+            n = 30
+            words = [get_random_word(randint(1, 30)) for _ in range(n)]
+            f.write("\n".join(words))
+        count += 1
 
 if __name__ == "__main__":
     main()            
